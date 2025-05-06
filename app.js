33295597183b69b450ed6fe309cca80c1ee0a145
Alejandro Arriaga <19030837@itcelaya.edu.mx>
2025-05-06 11:54:10 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  //req => request, la peticion
+  //res =>response, la respuesta
+  res.status(200).send("Hello World");
+});
+
+app.post("/team/pokemons", () => {
+  res.status(200).send("Hello World");
+});
+
+app.get("/team", () => {
+  res.send("Hello World");
+});
+
+app.delete("/team/pokemons/:pokeid", () => {
+  res.status(200).send("Hello World");
+});
+
+app.put("/team", () => {
+  res.status(200).send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log("Server started at port " + port);
+});
